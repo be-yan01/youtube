3d0ad729ee85541dbb850a14fa96417c14a6619b
be-yan <lab.imoby@gmail.com>
2020-07-26 01:05:24 +0900

    GUIでしきい値などを操作できるように改良。

--- Five_Nights_at_Freddys_4_Killer/Five_Nights_at_Freddys_4_Killer/Form1.Designer.cs
+++ Five_Nights_at_Freddys_4_Killer/Five_Nights_at_Freddys_4_Killer/Form1.Designer.cs
@@ -28,19 +28,32 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.button1 = new System.Windows.Forms.Button();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button2 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(1024, 567);
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button1.Location = new System.Drawing.Point(950, 46);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 0;
-            this.button1.Text = "音量最大化";
+            this.button1.Text = "音量ミュート";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
@@ -50,23 +63,131 @@
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
             this.pictureBox1.Location = new System.Drawing.Point(0, 0);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1129, 602);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.Size = new System.Drawing.Size(1118, 681);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(90, 24);
+            this.trackBar1.Maximum = 100;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(193, 45);
+            this.trackBar1.TabIndex = 2;
+            this.trackBar1.Value = 100;
+            this.trackBar1.Scroll += new System.EventHandler(this.trackBar1_Scroll);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(103, 46);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(168, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(78, 12);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "ピークしきい値：";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 57);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 12);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "ピーク現在値：";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label3.Location = new System.Drawing.Point(169, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(35, 12);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "label3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label4.Location = new System.Drawing.Point(169, 57);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 12);
+            this.label4.TabIndex = 7;
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button2
+            // 
+            this.button2.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button2.Location = new System.Drawing.Point(1031, 46);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 8;
+            this.button2.Text = "音量最大化";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(948, 18);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 12);
+            this.label5.TabIndex = 9;
+            this.label5.Text = "音量レベル：";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(1018, 18);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 12);
+            this.label6.TabIndex = 10;
+            this.label6.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1129, 602);
+            this.ClientSize = new System.Drawing.Size(1118, 681);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.button1);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "\"Five Nights at Freddy\'s 4\" Killer";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -74,6 +195,16 @@
 
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- Five_Nights_at_Freddys_4_Killer/Five_Nights_at_Freddys_4_Killer/Form1.cs
+++ Five_Nights_at_Freddys_4_Killer/Five_Nights_at_Freddys_4_Killer/Form1.cs
@@ -1,11 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using NAudio.CoreAudioApi;
 using NAudio.Wave;
@@ -15,10 +8,54 @@ namespace Five_Nights_At_Fleddys_Killer
 {
     public partial class Form1 : Form
     {
-        private delegate void SafeCallDelegate();
+        private delegate void ActivateFormDelegate();
+
+        WasapiLoopbackCapture capture = new WasapiLoopbackCapture();
+
+        float peakThreshold = 0.3f;
+        float currentPeak   = 0.0f;
+
         public Form1()
         {
             InitializeComponent();
+
+            capture.DataAvailable += (s, a) =>
+            {
+                var peak = 0.0f;
+                for (int i = 0; i < a.BytesRecorded; i = i + 4)
+                {
+                    float f = BitConverter.ToSingle(a.Buffer, 0);
+
+                    f = Math.Abs(f);
+
+                    if (peak < f)
+                    {
+                        peak = f;
+                    }
+                }
+                if (peakThreshold < 1.0f && peak > peakThreshold)
+                {
+                    setVolume(0.0f);
+                    var d = new ActivateFormDelegate(this.Activate);
+                    this.Invoke(d);
+                }
+
+                if (currentPeak < peak)
+                {
+                    currentPeak = peak;
+                }
+            };
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            trackBar1.Value = (int)(peakThreshold * 100.0f);
+            label3.Text = Float2String(peakThreshold);
+
+            capture.StartRecording();
+        }
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            capture.StopRecording();
         }
 
         private void activateWindow()
@@ -26,57 +63,54 @@ namespace Five_Nights_At_Fleddys_Killer
             this.Activate();
         }
 
-        private void volumeMute()
+        private void setVolume(float level)
         {
             MMDevice device;
             MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
             device = DevEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-            device.AudioEndpointVolume.MasterVolumeLevelScalar = 0.0f;
+            device.AudioEndpointVolume.MasterVolumeLevelScalar = level;
+
+            //label6.Text = ((int)(level * 100.0f)).ToString();
         }
 
-        private void maxVolume()
+        private void updateVolumeLavel()
         {
             MMDevice device;
             MMDeviceEnumerator DevEnum = new MMDeviceEnumerator();
             device = DevEnum.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
-            device.AudioEndpointVolume.MasterVolumeLevelScalar = 1.0f;
+            float level = device.AudioEndpointVolume.MasterVolumeLevelScalar;
+
+            label6.Text = ((int)(level * 100.0f)).ToString();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            maxVolume();
+            setVolume(0.0f);
         }
-
-        private void Form1_Load(object sender, EventArgs e)
+        private void button2_Click(object sender, EventArgs e)
         {
-            var capture = new WasapiLoopbackCapture();
-
-            capture.DataAvailable += (s, a) =>
-            {
-                var maxVol = 0.0f;
-                for (int i = 0; i < a.BytesRecorded; i = i + 4)
-                {
-                    float f = BitConverter.ToSingle(a.Buffer, 0);
-
-                    f = Math.Abs(f);
-
-                    if (maxVol < f)
-                    {
-                        maxVol = f;
-                    }
-                }
-                if (maxVol > 0.3f)
-                {
-                    var d = new SafeCallDelegate(activateWindow);
-                    this.Invoke(d);
-                    volumeMute();
-                    Console.WriteLine(maxVol);
-                }
-
-
-            };
-
-            capture.StartRecording();
+            setVolume(1.0f);
         }
+
+        private void trackBar1_Scroll(object sender, EventArgs e)
+        {
+            peakThreshold = (float)trackBar1.Value / 100.0f;
+            label3.Text = peakThreshold < 1.0f ? Float2String(peakThreshold) : "なし";
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Value = (int)(currentPeak * 100.0f + 0.5f);
+            label4.Text = Float2String(currentPeak);
+            currentPeak = 0.0f;
+
+            updateVolumeLavel();
+        }
+
+        private static string Float2String(float val)
+        {
+            return val.ToString("F2");
+        }
+
     }
 }
